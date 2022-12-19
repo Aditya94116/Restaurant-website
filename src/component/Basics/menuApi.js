d5a93c7b1c545bd98eadda2e60126935127fcc95
Aditya94116 <90316917+Aditya94116@users.noreply.github.com>
2022-12-19 21:23:32 +0530

    Add files via upload

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,89 @@
+const Menu=[{
+    id:1,
+    image: "https://im.rediff.com/getahead/2020/sep/29burnt-garlic-chilli-maggi.jpg",
+    name:"Maggi",
+    category: "breakfast",
+    price: "12₹",
+    description: "Maggie is best indian chatpata food quick and easy kids and adult favorites. Love this when no mood of cooking. You can find it any Indian store near you. It’s similar to Raman noodle just Indian spices make it better.",
+
+},
+
+{
+    id:2,
+    image:"https://rumkisgoldenspoon.com/wp-content/uploads/2022/10/Aloo-pakoda-1_01-Featured.jpg",
+    name:"aloo pakoda",
+    category: "evening",
+    price: "35₹",
+    description:" Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks. Make this awesome vegan potato snack in less than 30 minutes.",
+},
+
+{
+    id:3,
+    image:"https://www.simplyrecipes.com/thmb/c2WYSg5HoGxSsWvcgayWQMXv_2M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Cajun-Corn-LEAD-04-4c0471cde9484a3a86110bc408077d06.jpg",
+    name:"corn",
+    category:"breakfast",
+    price:"25₹",
+    description:"The seeds, which are also known as corn, are used as food for humans and livestock and as a source of biofuel and can be processed into a wide range of useful chemicals.",
+},
+
+
+{
+    id:4,
+    image:"https://bonmasala.com/wp-content/uploads/2022/06/Chole-Masala.jpg",
+    name:"chhola",
+    category:"lunch",
+    price:"20₹",
+    description:"Chole is the Indian name for chickpeas. Tossed in a savoury gravy and mixed with just the right spices, chickpeas get a new identity. This steamy gravy is the true soul mate for our Bhature.",
+},
+
+{
+    id:5,
+    image:"https://media-cdn.tripadvisor.com/media/photo-s/1b/3d/36/a0/fresh-hand-stretched.jpg",
+    name:"pizza",
+    category:"evening",
+    price:"80₹",
+    description:"pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot .",
+},
+
+
+{
+    id:6,
+    image:"https://foodsauda-cdn.s3.ap-south-1.amazonaws.com/Images/Menu/Thumb_non_veg_thali.jpg",
+    name:"non-veg thali",
+    category:"dinner",
+    price:"240₹",
+    description:"Chicken Thali will typically have a rice biryani featuring chicken, with a gravy dish with chicken, some starters, raita.",
+},
+
+{
+    id:7,
+    image:"https://kantisweets.com/media-uploader/since1636843357.jpeg",
+    name:"Sweets",
+    category:"dinner",
+    price:"50₹",
+    description:"The origin of sweets in the Indian subcontinent has been traced to at least 500 BCE when, records suggest, both raw sugar (gur, vellam, jaggery) and refined sugar (sarkara) were being produced.[19] By 300 BCE..",
+},
+
+{
+    id:8,
+    image:"https://sandhyahariharan.co.uk/wp-content/uploads/2010/11/Rajma-Masala-1-of-1-500x500.jpg",
+    name:"Rajma Rice",
+    category:"lunch",
+    price:"40₹",
+    description:"Rajma chawal is a simple yet delicious meal. Rajma is rich and iron and protein when it is combined with rice it is a great source of carbohydrates. So it makes a wholesome meal. It is great meal for kids lunch box too as it one of the favorite dish among the kids. ",
+},
+
+{
+    id:9,
+    image:"https://i0.wp.com/vegecravings.com/wp-content/uploads/2017/03/samosa-recipe-step-by-step-instructions.jpg?fit=1801%2C1717&quality=65&strip=all&ssl=1",
+    name:"samosa",
+    category:"evening",
+    price:"10₹",
+    description:"A samosa (/səˈmoʊsə/) or singara is a fried South Asian pastry with a savory filling, including ingredients such as spiced potatoes, onions, and peas. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.",
+},
+
+
+
+
+];
+export default Menu;
